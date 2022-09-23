10b8c3d22f9ebc7bd1d1e664e8858fb2f5ff62de
Student <league@iMac-22.attlocal.net>
2022-09-22 18:29:37 -0700

    Hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -2,6 +2,8 @@ package _03_Hangman;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayDeque;
 import java.util.Iterator;
 
@@ -11,46 +13,99 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Hangman implements ActionListener {
+public class Hangman implements ActionListener, KeyListener {
 	ArrayDeque<StringBuilder> wordbank = new ArrayDeque<>();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	JLabel tries = new JLabel();
 	JTextField box = new JTextField();
 	String random = "";
-	StringBuilder rbuild = new StringBuilder();
 	StringBuilder placeholder = new StringBuilder();
-	int numberGames = 0;
+	StringBuilder underscore;
+	int counter = 5;
 
-	public void run1() {
-		String games = JOptionPane.showInputDialog("How many games do you want to play? (Insert a numerical value)");
-		numberGames = Integer.parseInt(games);
-		run2();
-	}
-
-	public void run2() {
+	public void run() {
 		frame.add(panel);
 		panel.add(label);
+		panel.add(tries);
 		panel.add(box);
-		box.addActionListener(this);
-		for (int i = 0; i < numberGames; i++) {
-			random = Utilities.readRandomLineFromFile("dictionary.txt");
-			rbuild.append(random);
-			wordbank.push(rbuild);
-		}
+		// box.addActionListener(this);
+		box.addKeyListener(this);
+		box.setColumns(1);
+		random = Utilities.readRandomLineFromFile("dictionary.txt");
+		StringBuilder rbuild = new StringBuilder();
+		rbuild.append(random);
+		wordbank.push(rbuild);
+		underscore = new StringBuilder();
 		placeholder = wordbank.pop();
-		label.setText(wordbank.getFirst().replace(0, wordbank.size()-1, "_").toString());
+		System.out.println(placeholder.toString());
+		for (int i = 0; i < placeholder.length(); i++) {
+			underscore.append("_ ");
+		}
+		tries.setText("" + counter);
+		label.setText(underscore.toString());
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-
+		frame.pack();
 	}
 
 	public static void main(String[] args) {
-		new Hangman().run1();
+		new Hangman().run();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == e.VK_ENTER) {
+			if (box.getText().length() == 1) {
+				String answer;
+				answer = box.getText();
+				if (placeholder.indexOf(answer) != -1) {
+					for (int i = 0; i < placeholder.length(); i++) {
+						if (i == placeholder.indexOf(answer)) {
+							placeholder.replace(i, i + 1, "1");
+							underscore.replace(i * 2, i * 2 + 2, answer + " ");
+						}
+					}
+				}
+				else {
+					counter-=1;
+					tries.setText(""+counter);
+				}
+				box.setText("");
+				label.setText(underscore.toString());
+				if (counter == 0) {
+					System.exit(0);
+				}
+				if (underscore.indexOf("_") == -1) {
+					int pressed = JOptionPane.showConfirmDialog(null, "Do you wish to continue?");
+					if (pressed == JOptionPane.YES_OPTION) {
+						run();
+					}
+					else {
+						System.exit(0);
+					}
+				}
+
+			}
+
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
 	}
